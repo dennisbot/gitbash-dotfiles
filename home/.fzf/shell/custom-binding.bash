bfd92403bf53016343bde1b53a722bb4d578b282
Dennis Huillca <dennisbot@gmail.com>
2023-09-16 13:22:16 -0500

    fzf: sort by last updated date first

--- home/.fzf/shell/custom-binding.bash
+++ home/.fzf/shell/custom-binding.bash
@@ -1,8 +1,8 @@
 bind -x '"\C-s": "fzf_pickexec"'
 fzf_pickexec() {
-    local file=$(find . -type f -name "*.exe" | fzf --height ~60 --reverse)
+    local file=$(ls --color=never -t *.exe | fzf --height ~60 --reverse)
     if [ -n "$file" ]; then
-        READLINE_LINE="$file < input"
+        READLINE_LINE="./$file < input"
         READLINE_POINT=${#READLINE_LINE}
     fi
 }
