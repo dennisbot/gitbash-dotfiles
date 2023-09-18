73acbf315d5e137a81dc7a2e595f321763484977
Dennis Huillca <dennisbot@gmail.com>
2023-09-17 23:17:52 -0500

    modify fzf exec picker to pick automatically if there is one in the list

--- home/.fzf/shell/custom-binding.bash
+++ home/.fzf/shell/custom-binding.bash
@@ -1,8 +1,21 @@
 bind -x '"\C-s": "fzf_pickexec"'
 fzf_pickexec() {
-    local file=$(ls --color=never -t *.exe | fzf --height ~60 --reverse)
-    if [ -n "$file" ]; then
-        READLINE_LINE="./$file < input"
+    local files=($(ls --color=never -t *.exe | head -n 2))
+    local file
+
+    if [[ ${#files[@]} -gt 0 ]]; then
+        if [[ ${#files[@]} -gt 1 ]]; then
+            file=$(printf '%s\n' "${files[0]}" "${files[0]} < input" "${files[1]}" | fzf --height ~60 --reverse)
+        else
+            file=$(printf '%s\n' "${files[0]}" "${files[0]} < input" | fzf --height ~60 --reverse)
+        fi
+
+        if [[ "$file" == "${files[0]} < input" ]]; then
+            READLINE_LINE="./${files[0]} < input"
+        else
+            READLINE_LINE="./$file"
+        fi
+
         READLINE_POINT=${#READLINE_LINE}
     fi
 }
