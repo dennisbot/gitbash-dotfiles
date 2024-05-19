18d85f9025bd563be295a7e88d4cc77a5a871f6d
Dennis Huillca <dennisbot@gmail.com>
2024-05-19 00:04:03 -0500

    chore(readme,ps1): update readme and consolidate setup ps1 scripts into one

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
-# git-bash dotfiles setup (windows only)
+# git-bash dotfiles and windows-terminal symlink setup (windows only)
+
 ## PowerShell script for creating symbolic links
 
 setup.ps1 is a PowerShell script for creating symbolic links to all files and directories in a given directory (your home directory for the purpose of this project), excluding files with the extensions ".ps1" and ".md". 
 
-To run this script open a PowerShell with administrator rights and move to this repo location, then.
+1. Run `setup-symlinks.ps1` in powershell with admin rights:
 
-```powershell
-PS <path to your repo>.\home\setup-symlinks.ps1
-```
+  ```powershell
+  PS <path to this repo>.\home\setup-symlinks.ps1
+  ```
+
+  <div style="margin-left: 40px;">
+    <p>In case you get the following error:</p>
+    <img src="./Resources/powershell-policy-change.png" alt="plot"/>
+    <p>Fix it by running:</p>
+  </div>
+
+  ```powershell
+  Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope LocalMachine
+  ```
+  <div style="margin-left: 40px;">
+    <p>And go to step 1 again.</p>
+  </div>
 
 The script will do the following:
 
@@ -18,8 +32,25 @@ The script will do the following:
 5. If a symbolic link to the item already exists, the script will check if it is a symbolic link. If it is not, the existing file will be removed and a new symbolic link will be created.
 
 
-additionally, if you want to set msys2 mingw64 (to run C++ apps in windows), you need to install this same ps1 file but pointing to your new home location which for my case was `C:\msys64\home\dennisbot`, after that you will need to include some configurations in vscode for it to work properly in an embeded vscode shell.
+Additionally, if you want to set msys2 mingw64 (to run C++ apps in windows), you need to install this same ps1 file but pointing to your new home location which for my case was `C:\msys64\home\dennisbot`, after that you will need to include some configurations in vscode for it to work properly in an embeded vscode shell.
 
 see: https://github.com/dennisbot/cp
 
 use pacman to install git, fd and fzf in msys2 env
+
+## Aditional information regarding Windows Terminal
+
+The settings file for Windows Terminal can be found in your user profile directory. The specific location of the file depends on whether you installed Windows Terminal from the Microsoft Store or using preview version from Choco package manager.
+
+If you already have Windows Terminal installed in your windows, the settings file is located at:
+
+```bash
+%USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
+```
+Otherwise, we will install the preview version using Choco, the settings file is located at:
+
+```bash
+%USERPROFILE%\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json
+```
+
+Note that %USERPROFILE% is a environment variable that points to a directory. You can access that by typing it into the Windows search bar or by opening the Run dialog (Windows key + R) and typing it there.
---
+++ setup-symlinks.ps1
@@ -0,0 +1,155 @@
+if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+  # Install Chocolatey
+  Set-ExecutionPolicy Bypass -Scope Process -Force;
+  [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+$env:Path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine') + ';' + [System.Environment]::GetEnvironmentVariable('Path', 'User')
+
+# Check and install fd if not present
+if (-not (Get-Command fd -ErrorAction SilentlyContinue)) {
+  Write-Host "Installing fd..."
+  choco install fd -y
+}
+else {
+  Write-Host "fd is already installed."
+}
+
+# Check and install fzf if not present
+if (-not (Get-Command fzf -ErrorAction SilentlyContinue)) {
+  Write-Host "Installing fzf..."
+  choco install fzf -y
+}
+else {
+  Write-Host "fzf is already installed."
+}
+
+$installed = choco list --localonly | Where-Object { $_ -match "firacode" }
+
+if ($installed) {
+  Write-Host "Fira Code is already installed."
+}
+else {
+  choco install firacode -y
+}
+
+function Get-Windows-Terminal-Home-Path {
+  $storePath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe"
+  $previewPath = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe"
+
+  if (Test-Path $storePath) {
+      $wtLocalFilesFolder = $storePath
+  }
+  elseif (Test-Path $previewPath) {
+      $wtLocalFilesFolder = $previewPath
+  }
+  else {
+      # If Windows Terminal is not installed via Store or Preview, install it using Chocolatey
+      Write-Warning "Windows Terminal not found. Installing using Chocolatey..."
+
+      # Check if Chocolatey is installed
+      $chocoInstalled = $null -ne (Get-Command choco -ErrorAction SilentlyContinue)
+      if (-not $chocoInstalled) {
+          Write-Error "Chocolatey is not installed. Please install Chocolatey first."
+          return
+      }
+
+      # Install Windows Terminal using Chocolatey
+      if (-not (Get-Command wt -ErrorAction SilentlyContinue)) {
+          choco install microsoft-windows-terminal --pre -y
+
+          # Wait a moment for the installation to complete
+          Start-Sleep -Seconds 30
+      }
+
+      # Re-check if the path exists now after installation
+      if (Test-Path $storePath) {
+          $wtLocalFilesFolder = $storePath
+      }
+      elseif (Test-Path $previewPath) {
+          $wtLocalFilesFolder = $previewPath
+      }
+      else {
+          Write-Error "Failed to install Windows Terminal using Chocolatey or detect its path."
+          return
+      }
+  }
+
+  $windowsTerminalFolder = Join-Path -Path $wtLocalFilesFolder -ChildPath "LocalState"
+  return $windowsTerminalFolder
+}
+
+function Create-Symlinks {
+  param(
+    [string]$TargetFolder,
+    [string]$SourceFolder,
+    [bool]$RunDry = $false
+  )
+
+  # SETUP: Execute this in powershell with admin rights
+  Get-ChildItem -Path $SourceFolder | ForEach-Object {
+    $extension = [System.IO.Path]::GetExtension($_.Name)
+    if ($extension -eq ".ps1" -or $extension -eq ".md") {
+      continue
+    }
+
+    $linkPath = Join-Path $TargetFolder $_.FullName.Substring($SourceFolder.Length)
+
+    if (Test-Path $linkPath) {
+      Write-Host "removing $linkPath"
+      if (-not $RunDry) {
+        Remove-Item $linkPath -Recurse -Force
+      }
+    }
+
+    if (-not (Test-Path $linkPath)) {
+      Write-Host "linking $linkPath -> $($_.FullName)"
+      if (-not $RunDry) {
+        New-Item -ItemType SymbolicLink -Path $linkPath -Value $_.FullName
+      }
+    }
+  }
+}
+
+# Check if running as an administrator
+if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+  Write-Error "You need to run this script as an Administrator!"
+  Exit
+}
+
+# Prompt the user for an option
+$option = Read-Host "Do you want to run dry first? (Y/N)"
+$homeFolder = Read-Host "Enter the path to the home folder (leave blank to use $HOME):"
+# Use the provided home folder or fall back to $HOME
+if ($homeFolder -eq "") {
+  $homeFolder = $HOME
+  Write-Host "homeFolder provided is: $homeFolder"
+}
+$sourceDotFilesFolder = Join-Path -Path $PWD.Path -ChildPath "home"
+
+# Prompt the user if they also want to run the Windows Terminal script
+$runWindowsTerminal = Read-Host "Do you also want to run the Windows Terminal script? (Y/N)"
+$windowsTerminalFolder = Get-Windows-Terminal-Home-Path
+
+$sourceWindowsTerminalFolder = Join-Path -Path $PWD.Path -ChildPath "windows-terminal"
+
+# Check if the user selected "Y"
+if ($option -eq "Y") {
+  Write-Host "running dry ..."
+  Create-Symlinks -TargetFolder $homeFolder -SourceFolder $sourceDotFilesFolder -RunDry $true
+  if ($runWindowsTerminal -eq "Y") {
+    Create-Symlinks -TargetFolder $windowsTerminalFolder -SourceFolder $sourceWindowsTerminalFolder -RunDry $true
+  }
+  Write-Host "run dry has finished"
+  # Perform some action
+}
+else {
+  Create-Symlinks -TargetFolder $homeFolder -SourceFolder $sourceDotFilesFolder -RunDry $false
+  if ($runWindowsTerminal -eq "Y") {
+    Create-Symlinks -TargetFolder $windowsTerminalFolder -SourceFolder $sourceWindowsTerminalFolder -RunDry $false
+  }
+}
+
+
+
