53a4b6f7994908893c3efb2c18848505d01a60ed
Dennis Huillca <dennisbot@gmail.com>
2026-08-31 15:33:41 -0500

    style(git-prompt): simplify prompt structure and adjust branch display format

--- home/.config/git/git-prompt.sh
+++ home/.config/git/git-prompt.sh
@@ -1,14 +1,7 @@
 PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
+PS1="$PS1"'\[\033[01;31m\]\t'   # time
 PS1="$PS1"'\n'                 # new line
-TIME='\[\033[01;31m\]\t \[\033[01;32m\]'
-USER='\u ';
-# HOST='\033[02;36m\]\h \033[01;34m\]'
-HOST='\[\033[01;36m\]\h '
-
-PS1="$TIME"
-PS1="$PS1$USER"
-PS1="$PS1$HOST"
-PS1="$PS1"'\w \[\033[01;33m\]'                 # current working directory
+PS1="$PS1"'\[\033[01;36m\]\w'   # current working directory
 if test -z "$WINELOADERNOEXEC"
 then
   GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -20,9 +13,9 @@ then
     . "$COMPLETION_PATH/git-completion.bash"
     . "$COMPLETION_PATH/git-prompt.sh"
     PS1="$PS1"'\[\033[00;33m\]'  # change color to yellow
-    PS1="$PS1"'`__git_ps1 [%s]`'   # bash function
+    PS1="$PS1"'`__git_ps1 "\n[%s]"`'  # branch on its own line
   fi
 fi
 PS1="$PS1"'\[\033[0m\]'        # change color
-# PS1="$PS1"'\n'                 # new line
+PS1="$PS1"'\n'                 # new line
 PS1="$PS1"'$ '                 # prompt: always $
